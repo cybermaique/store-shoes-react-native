3b09a7b9a9cae79e3bdb5cbe995991b1546a6627
cybermaique <maiqued.18@gmail.com>
2022-02-03 19:27:56 -0930

    title detail

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Detail() {
- return (
+//quando navegamos em uma página, recebemos o parâmetro navigation, e dentro dele temos o sertoptions, que podemos passar algumas propriedades, como o settitle para alterar o titulo do header.
+
+export default function Detail({ navigation }) {
+ 
+  navigation.setOptions({
+ headerTitle: 'Nike Air 10'
+})
+
+  return (
     <View>
         <Text>Page Detail</Text>
     </View>
